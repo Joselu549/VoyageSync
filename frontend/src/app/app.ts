7318cf2d255c39c713e68fe1e7c6d0f02af58d32
José Luis Abellán Monreal <joseluisabellanmonreal@gmail.com>
2026-06-25 19:40:00 +0200

    test: prueba ordenación y pre-commit
    # feat: CTB-1 add hat wobble
    # ^--^  ^---^ ^-------------^
    # |     |     |
    # |     |     +-> Summary in present tense.
    # |     +-> Jira ID Task
    # +-> Type: chore, docs, feat, fix, refactor, style, or test.
    
    # feat: new feature for the user, not a new feature for build script
    # fix: bug fix for the user, not a fix to a build script
    # docs: changes to the documentation
    # style: formatting, missing semi colons, etc; no production code change
    # refactor: refactoring production code, eg. renaming a variable
    # test: adding missing tests, refactoring tests; no production code change
    # chore: updating grunt tasks etc; no production code change
    # ci: updating scripts for continuous integration
    # build: update building scripts or Docker Images
    # perf: update code for performance improvement
    # revert: revert changes

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,12 @@
+import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected readonly title = signal('frontend');
+}
