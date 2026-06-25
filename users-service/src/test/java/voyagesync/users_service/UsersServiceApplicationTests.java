d6b04fe01bbd365d34af0f99f1307b181b594fa2
José Luis Abellán Monreal <joseluisabellanmonreal@gmail.com>
2026-06-25 20:21:32 +0200

    feat: Servicio prueba Java
    # feat: Cuando agregues una nueva funcionalidad o característica que el usuario final pueda ver o usar
    # fix: Utilízalo para la corrección de errores puntuales o bugs en el código
    # chore: Aplícalo para tareas rutinarias y de mantenimiento interno que no afectan al usuario, como actualizar dependencias, librerías, configuraciones del proyecto o herramientas de desarrollo
    # test: Úsalo estrictamente para añadir, modificar o corregir pruebas automatizadas (unitarias, de integración, etc.) sin alterar el código de producción
    # refactor: Aplícalo cuando reestructures o limpies el código para mejorarlo, pero sin añadir funciones ni arreglar bugs
    # docs: Úsalo únicamente para cambios en la documentación, como el archivo README, manuales o comentarios internos.

---
+++ users-service/src/test/java/voyagesync/users_service/UsersServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package voyagesync.users_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
